4470b95d712d7493d69e0c39bfe9412032baec84
usamaghazi4711 <mughazi4711@gmail.com>
2026-01-23 18:55:09 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Use relative paths so Express can find the files
+  build: {
+    outDir: 'dist', // Ensures the output folder matches your server.js
+    emptyOutDir: true,
+  }
 })
